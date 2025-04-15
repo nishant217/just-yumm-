1201433a5ce346d07f45ea897cd4e78d005fba7a
Nishant_Sharma <ns902709@gmail.com>
2025-04-15 22:08:28 +0530

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^!oujxy5z(eawqr#h7q@ru^9zu)v14%=x1g0qdpgf$(t%ydp$_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','just-yumm-1.onrender.com']
+ALLOWED_HOSTS = ['*','.vercel.app','just-yumm-1.onrender.com']
 CSRF_TRUSTED_ORIGINS = [
     'https://just-yumm-1.onrender.com',
 ]
